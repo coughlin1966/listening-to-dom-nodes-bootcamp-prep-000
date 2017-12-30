076a3e07f1fe23644216225a83985dd150a7467a
Peter Coughlin <coughlin1966@gmail.com>
2017-12-30 02:37:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const input = document.querySelector('input')
 input.addEventListener('keydown', function(e) {
     if (e.which === 71) {
         return e.preventDefault()
-              
+
     }
 })
 let divs = document.querySelectorAll('div')
@@ -32,10 +32,10 @@ function capture(e) {
 */
 function bubble(e) {
   e.stopPropagation()
- 
+
   console.log(this.firstChild.nodeValue.trim() + ' bubbled')
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', bubble)
 }
